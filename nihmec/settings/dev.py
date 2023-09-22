369816368bc941c16da271a8a624a6905569a419
Fleissen <p.lekia@fleissen.com>
2023-09-22 12:34:09 +0100

    testing db

--- nihmec/settings/dev.py
+++ nihmec/settings/dev.py
@@ -2,7 +2,7 @@ from .base import *
 from django.core.management.utils import get_random_secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
@@ -10,8 +10,22 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["*"]
 
+EMAIL_HOST_USER = os.getenv('DEFAULT_FROM_EMAIL')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#     }
+# }
+
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+
+PAYSTACK_SECRET_KEY = os.getenv('PAYSTACK_SECRET_KEY')
 
 try:
     from .local import *
--- nihmec/settings/production.py
+++ nihmec/settings/production.py
@@ -4,8 +4,17 @@ from django.core.management.utils import get_random_secret_key
 import dj_database_url
 
 ALLOWED_HOSTS = ["nihmec-production.up.railway.app", "nihmec.com"]
-DEBUG = False
+DEBUG = env('DEBUG')
+EMAIL_HOST_USER = env('DEFAULT_FROM_EMAIL')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
 
+
+AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+
+
+PAYSTACK_SECRET_KEY = env('PAYSTACK_SECRET_KEY')
 try:
     from .local import *
 except ImportError:
